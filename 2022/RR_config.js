cefd77102ddfab7081b022f5d1f8adb22e78a154
Aidan Jarrett <33237964+aidanw02@users.noreply.github.com>
2022-03-22 20:12:42 -0500

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022TulsaReginol",
+        "defaultValue":"2020ilch",
         "required":"true",
         "disabled":"true"
       },
