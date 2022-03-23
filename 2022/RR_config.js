cbf54397792ff870f929b27d8359a4a10fec3edf
Aidan Jarrett <33237964+aidanw02@users.noreply.github.com>
2022-03-22 20:24:14 -0500

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2020ilch",
+        "defaultValue":"2020flwp",
         "required":"true",
         "disabled":"true"
       },
