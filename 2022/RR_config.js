6a30807b8303db85c292d023e69ab911b2df9eb0
Aidan Jarrett <33237964+aidanw02@users.noreply.github.com>
2022-04-07 09:37:27 -0500

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022mokc",
+        "defaultValue":"2022oktu",
         "required":"true",
         "disabled":"true"
       },
